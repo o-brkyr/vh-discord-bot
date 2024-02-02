b013ed5a7ea9442231d1eb214abdbc63f71f22a4
o-brkyr <oliverdbarker@gmail.com>
2024-02-02 00:28:23 +0000

    Add constants

---
+++ discord_bot/constants.py
@@ -0,0 +1,19 @@
+from custom_types import Status
+
+
+CHANNEL_TITLE = "𝖛𝖆𝖑𝖍𝖊𝖎𝖒"
+
+STATUS_ONLINE = "🟢"
+STATUS_OFFLINE = "🔴"
+STATUS_INBETWEEN = "🟠"
+STATUS_DEAD = "⚫"
+
+ROLE_NAME = "𝔳𝔞𝔩𝔥𝔢𝔦𝔪𝔢𝔯"
+
+
+STATUS_TO_SYMBOL_MAP: dict[Status, str] = {
+    Status.OFFLINE: STATUS_OFFLINE,
+    Status.ONLINE: STATUS_ONLINE,
+    Status.STARTING: f"{STATUS_OFFLINE} -> {STATUS_ONLINE}",
+    Status.STOPPING: f"{STATUS_ONLINE} -> {STATUS_OFFLINE}",
+}
