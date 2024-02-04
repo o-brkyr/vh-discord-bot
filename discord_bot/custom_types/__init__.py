816d3ff8e48d820f3951f9c69821050b7ac683ef
o-brkyr <oliverdbarker@gmail.com>
2024-02-04 00:04:27 +0000

    Cut unused constants

--- discord_bot/custom_types/__init__.py
+++ discord_bot/custom_types/__init__.py
@@ -1,22 +1,8 @@
 from enum import Enum, auto
-from typing import NamedTuple, Optional
-
-
-class Action(Enum):
-    JOIN = auto
-    LEAVE = auto
-    START = auto
-
-
-class Message(NamedTuple):
-    action: Action
-    uid: int
-    name: Optional[str]
-    time: Optional[int]
 
 
 class Status(Enum):
-    OFFLINE = auto
-    ONLINE = auto
-    STARTING = auto
-    STOPPING = auto
+    STOPPED = auto()
+    STARTED = auto()
+    STARTING = auto()
+    STOPPING = auto()
